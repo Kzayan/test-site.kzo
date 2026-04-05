c686acdd11904aa380240712c8712f29d33387e0
Kzayan <kzayan161@gmail.com>
2026-04-05 09:32:28 +0500

    Create frontend/src/components/Footer.js

---
+++ frontend/frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <p>© 2026 QWIZZ — Білім ойыны. Жасаған — <a href="#">Ayan KZ</a> 💙</p>
+  </footer>
+);
+
+export default Footer;
