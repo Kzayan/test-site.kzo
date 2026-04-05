9f3bdff6d49e23f0de42463c5276925951ca0ff3
Kzayan <kzayan161@gmail.com>
2026-04-05 09:38:33 +0500

    Delete Footer.js

--- frontend/frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="footer">
-    <p>© 2026 QWIZZ — Білім ойыны. Жасаған — <a href="#">Ayan KZ</a> 💙</p>
-  </footer>
-);
-
-export default Footer;
